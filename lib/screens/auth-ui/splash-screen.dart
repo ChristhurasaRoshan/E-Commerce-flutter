8722be477144a94b8939608d73b3d5689dd032ee
Roshan <romeoroshan@gmail.com>
2024-04-03 12:33:29 +0530

    change

--- lib/screens/auth-ui/splash-screen.dart
+++ lib/screens/auth-ui/splash-screen.dart
@@ -6,7 +6,9 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
+      backgroundColor: Colors.pink,
       appBar: AppBar(
         backgroundColor: Colors.pink,
         elevation: 0,
@@ -15,7 +17,17 @@ class SplashScreen extends StatelessWidget {
       body: Container(
         child: Column(
           children: [
-            Container(child: Lottie.asset("images/splash-icon.json"),)
+            Container(child: Lottie.asset("images/splash-icon.json")
+            ),
+            Container(
+              child: Text("Online Shop" , 
+              style:TextStyle(fontSize: 23,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontFamily: 'Poppins'
+              ),
+            ),
+            )
           ],
         ),
       ),
