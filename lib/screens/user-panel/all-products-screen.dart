a9446dde6216c58111e3b8ee7d228b55379bca3b
Shanuga123 <mshanuga2000@gmail.com>
2024-04-17 10:05:25 +0530

    changed

--- lib/screens/user-panel/all-products-screen.dart
+++ lib/screens/user-panel/all-products-screen.dart
@@ -1,25 +1,19 @@
-// ignore_for_file: file_names, prefer_const_constructors, sized_box_for_whitespace, avoid_unnecessary_containers
+// ignore_for_file: file_names, prefer_const_constructors, avoid_unnecessary_containers, sized_box_for_whitespace, prefer_const_literals_to_create_immutables, unnecessary_string_interpolations, prefer_interpolation_to_compose_strings
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_counter_app/screens/user-panel/single-category-products-screen.dart';
+import 'package:flutter_counter_app/models/product-model.dart';
 import 'package:flutter_counter_app/utils/app-constant.dart';
 import 'package:flutter/cupertino.dart';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_card/image_card.dart';
 
-import '../../models/categories-model.dart';
+import 'product-deatils-screen.dart';
 
-class AllCategoriesScreen extends StatefulWidget {
-  const AllCategoriesScreen({super.key});
+class AllProductsScreen extends StatelessWidget {
+  const AllProductsScreen({super.key});
 
-  @override
-  State<AllCategoriesScreen> createState() => _AllCategoriesScreenState();
-}
-
-class _AllCategoriesScreenState extends State<AllCategoriesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,12 +23,15 @@ class _AllCategoriesScreenState extends State<AllCategoriesScreen> {
         ),
         backgroundColor: AppConstant.appMainColor,
         title: Text(
-          "All Categories",
+          'All Products',
           style: TextStyle(color: AppConstant.appTextColor),
         ),
       ),
       body: FutureBuilder(
-        future: FirebaseFirestore.instance.collection('categories').get(),
+        future: FirebaseFirestore.instance
+            .collection('products')
+            .where('isSale', isEqualTo: false)
+            .get(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (snapshot.hasError) {
             return Center(
@@ -52,7 +49,7 @@ class _AllCategoriesScreenState extends State<AllCategoriesScreen> {
 
           if (snapshot.data!.docs.isEmpty) {
             return Center(
-              child: Text("No category found!"),
+              child: Text("No products found!"),
             );
           }
 
@@ -63,39 +60,59 @@ class _AllCategoriesScreenState extends State<AllCategoriesScreen> {
               physics: BouncingScrollPhysics(),
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
-                mainAxisSpacing: 3,
-                crossAxisSpacing: 3,
-                childAspectRatio: 1.19,
+                mainAxisSpacing: 5,
+                crossAxisSpacing: 5,
+                childAspectRatio: 0.80,
               ),
               itemBuilder: (context, index) {
-                CategoriesModel categoriesModel = CategoriesModel(
-                  categoryId: snapshot.data!.docs[index]['categoryId'],
-                  categoryImg: snapshot.data!.docs[index]['categoryImg'],
-                  categoryName: snapshot.data!.docs[index]['categoryName'],
-                  createdAt: snapshot.data!.docs[index]['createdAt'],
-                  updatedAt: snapshot.data!.docs[index]['updatedAt'],
+                final productData = snapshot.data!.docs[index];
+                ProductModel productModel = ProductModel(
+                  productId: productData['productId'],
+                  categoryId: productData['categoryId'],
+                  productName: productData['productName'],
+                  categoryName: productData['categoryName'],
+                  salePrice: productData['salePrice'],
+                  fullPrice: productData['fullPrice'],
+                  productImages: productData['productImages'],
+                  deliveryTime: productData['deliveryTime'],
+                  isSale: productData['isSale'],
+                  productDescription: productData['productDescription'],
+                  createdAt: productData['createdAt'],
+                  updatedAt: productData['updatedAt'],
                 );
+
+                // CategoriesModel categoriesModel = CategoriesModel(
+                //   categoryId: snapshot.data!.docs[index]['categoryId'],
+                //   categoryImg: snapshot.data!.docs[index]['categoryImg'],
+                //   categoryName: snapshot.data!.docs[index]['categoryName'],
+                //   createdAt: snapshot.data!.docs[index]['createdAt'],
+                //   updatedAt: snapshot.data!.docs[index]['updatedAt'],
+                // );
                 return Row(
                   children: [
                     GestureDetector(
-                      onTap: () => Get.to(() => AllSingleCategoryProductsScreen(
-                            categoryId: categoriesModel.categoryId,
-                          )),
+                      onTap: () => Get.to(() =>
+                          ProductDetailsScreen(productModel: productModel)),
                       child: Padding(
                         padding: EdgeInsets.all(8.0),
                         child: Container(
                           child: FillImageCard(
                             borderRadius: 20.0,
                             width: Get.width / 2.3,
-                            heightImage: Get.height / 10,
+                            heightImage: Get.height / 6,
                             imageProvider: CachedNetworkImageProvider(
-                              categoriesModel.categoryImg,
+                              productModel.productImages[0],
                             ),
                             title: Center(
                               child: Text(
-                                categoriesModel.categoryName,
+                                productModel.productName,
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
                                 style: TextStyle(fontSize: 12.0),
                               ),
+                            ),
+                            footer: Center(
+                              child: Text("PKR: " + productModel.fullPrice),
                             ),
                           ),
                         ),
@@ -105,16 +122,6 @@ class _AllCategoriesScreenState extends State<AllCategoriesScreen> {
                 );
               },
             );
-
-            // Container(
-            //   height: Get.height / 5.0,
-            //   child: ListView.builder(
-            //     itemCount: snapshot.data!.docs.length,
-            //     shrinkWrap: true,
-            //     scrollDirection: Axis.horizontal,
-
-            //   ),
-            // );
           }
 
           return Container();
--- lib/screens/user-panel/main-screen.dart
+++ lib/screens/user-panel/main-screen.dart
@@ -1,22 +1,24 @@
 // ignore_for_file: file_names, prefer_const_constructors, no_leading_underscores_for_local_identifiers, avoid_unnecessary_containers, prefer_const_literals_to_create_immutables
 
-
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_counter_app/screens/user-panel/all-flash-sale-products.dart';
+import 'package:flutter_counter_app/screens/user-panel/all-products-screen.dart';
+import 'package:flutter_counter_app/screens/user-panel/cart-screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_counter_app/screens/auth-ui/welcome-screen.dart';
-import 'package:flutter_counter_app/utils/app-constant.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_counter_app/widgets/banner-widget.dart';
-import 'package:flutter_counter_app/widgets/category-widget.dart';
-import 'package:flutter_counter_app/widgets/custom-drawer-widget.dart';
-import 'package:flutter_counter_app/widgets/flash-sale-widget.dart';
-import 'package:flutter_counter_app/widgets/heading-widget.dart';
 import 'package:get/get.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import '../../utils/app-constant.dart';
+import '../../widgets/all-products-widget.dart';
+import '../../widgets/banner-widget.dart';
+import '../../widgets/category-widget.dart';
+import '../../widgets/custom-drawer-widget.dart';
+import '../../widgets/flash-sale-widget.dart';
+import '../../widgets/heading-widget.dart';
+import 'all-categories-screen.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
-@override
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -32,17 +34,11 @@ class MainScreen extends StatelessWidget {
         centerTitle: true,
         actions: [
           GestureDetector(
-            onTap: () async{
-              GoogleSignIn googleSignIn=GoogleSignIn();
-              FirebaseAuth _auth=FirebaseAuth.instance; 
-              await _auth.signOut();
-              await googleSignIn.signOut();
-              Get.offAll(()=>WelcomeScreen());
-            },
+            onTap: () => Get.to(() => CartScreen()),
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Icon(
-                Icons.logout,
+                Icons.shopping_cart,
               ),
             ),
           ),
@@ -64,35 +60,35 @@ class MainScreen extends StatelessWidget {
               HeadingWidget(
                 headingTitle: "Categories",
                 headingSubTitle: "According to your budget",
-                onTap: () {},
+                onTap: () => Get.to(() => AllCategoriesScreen()),
                 buttonText: "See More >",
               ),
 
               CategoriesWidget(),
 
-              // //heading
+              //heading
               HeadingWidget(
                 headingTitle: "Flash Sale",
                 headingSubTitle: "According to your budget",
-                onTap: () {},
+                onTap: () => Get.to(() => AllFlashSaleProductScreen()),
                 buttonText: "See More >",
               ),
 
               FlashSaleWidget(),
 
-              // //heading
-              // HeadingWidget(
-              //   headingTitle: "All Products",
-              //   headingSubTitle: "According to your budget",
-              //   onTap: () => Get.to(() => AllProductsScreen()),
-              //   buttonText: "See More >",
-              // ),
+              //heading
+              HeadingWidget(
+                headingTitle: "All Products",
+                headingSubTitle: "According to your budget",
+                onTap: () => Get.to(() => AllProductsScreen()),
+                buttonText: "See More >",
+              ),
 
-              // AllProductsWidget(),
+              AllProductsWidget(),
             ],
           ),
         ),
       ),
-      );
-  }}
-        
+    );
+  }
+}
